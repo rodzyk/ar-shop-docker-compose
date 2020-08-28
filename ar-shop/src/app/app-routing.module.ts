00c263ad5ccdce725866c50028537fcbb0748ea0
Andrii Rodzyk <rodzyk@programmer.net>
2020-08-28 16:34:12 +0300

    update app component

--- ar-shop/src/app/app-routing.module.ts
+++ ar-shop/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import("src/app/products/products.module").then((m) => m.ProductsModule),
+  },
+  {
+    path: 'cart',
+    loadChildren: () =>
+      import("src/app/cart/cart.module").then((m) => m.CartModule),
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
